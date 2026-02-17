dbc504766f26a73f6a1eb8a06e5bae4131952c08
Phantomer-soft <kucuktas255@gmail.com>
2026-02-17 15:08:23 +0300

    migration ekledim

--- KahootMvc/Program.cs
+++ KahootMvc/Program.cs
@@ -22,7 +22,11 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
         .AllowAnyHeader()                  // Tüm başlıklara izin ver
         .AllowCredentials()));      
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
